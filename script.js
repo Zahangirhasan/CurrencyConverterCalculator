0e3bbd35ec26b09432acf3cc0d1c7a3f9e9e28b7
Zahangirhasan <48876808+Zahangirhasan@users.noreply.github.com>
2021-06-20 01:49:50 +0600

    Currency Converter Calculator-created

---
+++ script.js
@@ -0,0 +1,41 @@
+const currencyElementOne = document.getElementById('currency-one');
+const amountElementOne = document.getElementById('amount-one');
+const currencyElementTwo = document.getElementById('currency-two');
+const amountElementTwo = document.getElementById('amount-two');
+
+const rateElement = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// fetch exchange rates and update the DOM
+
+function calculate() {
+  const currencyOne = currencyElementOne.value;
+  const currencyTwo = currencyElementTwo.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data)
+      const rate = data.rates[currencyTwo];
+      console.log(rate);
+
+      rateElement.innerText = `1 ${currencyOne} = ${rate} ${currencyTwo}`;
+      amountElementTwo.value = (amountElementOne.value * rate).toFixed(2);
+    });
+}
+
+// Event listeners
+
+currencyElementOne.addEventListener('change', calculate);
+amountElementOne.addEventListener('input', calculate);
+currencyElementTwo.addEventListener('change', calculate);
+amountElementTwo.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyElementOne.value;
+  currencyElementOne.value = currencyElementTwo.value;
+  currencyElementTwo.value = temp;
+  calculate()
+});
+
+calculate();
